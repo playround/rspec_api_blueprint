c07f3e828f905826ce78288cd98888a6dd423479
Petr Bela <bela.petr@gmail.com>
2014-01-26 22:32:00 -0800

    feat: copy markdown docs from source files

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Rspec Api Blueprint
+# RSpec API Blueprint
 
-Autogeneration of API documentation using the Blueprint format from request specs.
+Autogenerate API documentation in API blueprint format from request specs.
 
-You can find more about Blueprint at http://apiblueprint.org
+You can find more about API blueprint at http://apiblueprint.org
 
 ## Installation
 
@@ -26,25 +26,29 @@ In your spec_helper.rb file add
 
 Write tests using the following convention:
 
-- Top level descriptions are named after the model (plural form) followed by the word “Requests”. For a example model called Arena it would be “Arenas Requests”.
-- Second level descriptions are actions in the form of “VERB path”. For the show action of the Arenas controller it would be “GET /arenas/{id}”.
+- Resource descriptions are named "Group" followed by the resource name. E.g. for model called Arena it would be `Group Arena`.
+- Action descriptions are in the form of “VERB path”. For the show action of the arenas controller it would be `GET /v1/arenas/{id}`.
 
 Example:
 
-    describe 'Arenas Requests' do
-      describe 'GET /v1/arenas/{id}' do
-        it 'responds with the requested arena' do
-          arena = create :arena, foursquare_id: '5104'
-          get v1_arena_path(arena)
+```ruby
+describe 'Group Arena' do
+  describe 'GET /v1/arenas/{id}' do
+    it 'responds with the requested arena' do
+      arena = create :arena, foursquare_id: '5104'
+      get v1_arena_path(arena)
 
-          response.status.should eq(200)
-        end
-      end
+      response.status.should eq(200)
     end
+  end
+end
+```
 
 The output:
 
-    # GET /v1/arenas/{id}
+    # Group Arena
+
+    ## GET /v1/arenas/{id}
 
     + Response 200 (application/json)
 
@@ -58,11 +62,113 @@ The output:
           }
         }
 
+### Documentation
+
+The generator can also take documentation from your source files and insert it into the resulting blueprint. It will be copied as-is from the comments, so you can use markdown, `+ Parameters`, and anything else that the [blueprint specification](https://github.com/apiaryio/api-blueprint/blob/master/API%20Blueprint%20Specification.md) supports.
+
+Documentation for a resource is taken from a model file, and documentation for an action is taken from a controller file (see [configuration](#configuration)).
+
+Example:
+
+In `app/models/arena.rb`:
+
+```ruby
+# Arena represents a combat room.
+#
+# Attributes:
+#
+# - `name` - arena name
+# - `foursquare_id`
+class Arena < ActiveRecord::Base
+  attr_accessible :name, :foursquare_id
+end
+```
+
+In `app/controllers/arenas_controller.rb`:
+
+```ruby
+class ArenasController < ApplicationController
+
+  # GET /v1/arenas/{id}
+  # Fetch information about an arena.
+  #
+  # + Parameters
+  #   + id (integer, `1`) ... arena id
+  def show
+    @arena = Arena.find(params[:id])
+  end
+
+end
+```
+
+Output:
+
+    # Group Arena
+
+    Arena represents a combat room.
+
+    Attributes:
+
+    - `name` - arena name
+    - `foursquare_id`
+
+    ## GET /v1/arenas/{id}
+    Fetch information about an arena.
+
+    + Parameters
+      + id (integer, `1`) ... arena id
+
+    + Response 200 (application/json)
+
+        {
+          "arena": {
+            "id": "4e9dbbc2-830b-41a9-b7db-9987735a0b2a",
+            "name": "Clinton St. Baking Co. & Restaurant",
+            "latitude": 40.721294,
+            "longitude": -73.983994,
+            "foursquare_id": "5104"
+          }
+        }
+
+### Configuration
+
+You can customize paths and other behaviour in the RSpec config block in `spec_helper.rb`.
+
+Example:
+
+```ruby
+RSpec.configure do |config|
+  config.api_docs_output = './api_docs/generated'
+end
+```
+
+Configuration options:
+
+- `config.api_docs_output` sets the destination folder where the docs will be generated (default is `./api_docs`)
+
+- `config.api_docs_controllers` folder where to look for action docs (default is `./app/controllers`).
+
+  Can be passed a Proc to map resource name to file name:
+
+  ```ruby
+  config.api_docs_controllers = ->(resource) { "./app/controllers/#{resource}s_controller.rb" }
+  ```
+
+- `config.api_docs_models` folder where to look for model (resource) docs (default is `./app/models`).
+
+  Can be passed a Proc to map resource name to file name:
+
+  ```ruby
+  config.api_docs_models = ->(resource) { "./app/models/#{resource}.rb" }
+  ```
+
+- `config.api_docs_whitelist` -- by default, docs for all examples will be generated. If you want to only generate docs for selected examples, set `api_docs_whitelist = true` and then add `it "...", docs: true do ... end` to each example.
+
+- If you want to ensure the order of requests in the generated docs, set `config.order = 'default'` (or run as `rspec --order default`).
+
 ## Caveats
 
-401, 403 and 301 statuses are ignored since rspec produces a undesired output.
-
-TODO: Add option to choose ignored statuses.
+- 401, 403 and 301 statuses are ignored since rspec produces a undesired output. TODO: Add option to choose ignored statuses.
 
 ## Contributing
 
@@ -71,3 +177,9 @@ TODO: Add option to choose ignored statuses.
 3. Commit your changes (`git commit -am 'Add some feature'`)
 4. Push to the branch (`git push origin my-new-feature`)
 5. Create new Pull Request
+
+### Test
+
+    $ cucumber
+
+(in progress)
--- lib/rspec_api_blueprint.rb
+++ lib/rspec_api_blueprint.rb
@@ -3,71 +3,183 @@ require "rspec_api_blueprint/string_extensions"
 
 
 RSpec.configure do |config|
+  config.add_setting :api_docs_output, default: 'api_docs'
+  config.add_setting :api_docs_controllers, default: 'app/controllers'
+  config.add_setting :api_docs_models, default: 'app/models'
+  config.add_setting :api_docs_whitelist, default: false
+
+  api_docs_folder_path = nil
+  touched_files = {}
+
   config.before(:suite) do
     if defined? Rails
-      api_docs_folder_path = File.join(Rails.root, '/api_docs/')
+      api_docs_folder_path = File.join(Rails.root, config.api_docs_output)
     else
-      api_docs_folder_path = File.join(File.expand_path('.'), '/api_docs/')
+      api_docs_folder_path = File.join(File.expand_path('.'), config.api_docs_output)
     end
 
     Dir.mkdir(api_docs_folder_path) unless Dir.exists?(api_docs_folder_path)
-
-    Dir.glob(File.join(api_docs_folder_path, '*')).each do |f|
-      File.delete(f)
-    end
   end
 
+  RESOURCE_GROUP = /Group\s(\w+)/
+  ACTION_GROUP = /(GET|POST|PATCH|PUT|DELETE)\s(.+)$/
+
   config.after(:each, type: :request) do
-    if response
-      example_group = example.metadata[:example_group]
-      example_groups = []
+    next if example.metadata[:docs] == false
+    next if config.api_docs_whitelist && !example.metadata[:docs]
 
-      while example_group
-        example_groups << example_group
-        example_group = example_group[:example_group]
-      end
+    response ||= last_response
+    request ||= last_request
 
-      action = example_groups[-2][:description_args].first if example_groups[-2]
-      example_groups[-1][:description_args].first.match(/(\w+)\sRequests/)
-      file_name = $1.underscore
+    next unless response
 
-      if defined? Rails
-        file = File.join(Rails.root, "/api_docs/#{file_name}.txt")
-      else 
-        file = File.join(File.expand_path('.'), "/api_docs/#{file_name}.txt")
-      end
+    action_group, action_match = find_group(example.metadata[:example_group], ACTION_GROUP)
+    action_comment = parse_action_comment(action_group, config.api_docs_controllers)
 
-      File.open(file, 'a') do |f|
-        # Resource & Action
-        f.write "# #{action}\n\n"
+    next unless action_group
 
-        # Request
-        request_body = request.body.read
-        authorization_header = request.headers['Authorization']
+    resource_group, resource_match = find_group(action_group, RESOURCE_GROUP)
+    resource_comment = parse_resource_comment(resource_group, config.api_docs_models)
 
-        if request_body.present? || authorization_header.present?
-          f.write "+ Request #{request.content_type}\n\n"
+    next unless resource_group
 
-          # Request Headers
-          if authorization_header.present?
-            f.write "+ Headers\n\n".indent(4)
-            f.write "Authorization: #{authorization_header}\n\n".indent(12)
-          end
+    resource_name = resource_match[1]
+    file_name = resource_name.underscore
 
-          # Request Body
-          if request_body.present? && request.content_type == 'application/json'
-            f.write "+ Body\n\n".indent(4) if authorization_header
-            f.write "#{JSON.pretty_generate(JSON.parse(request_body))}\n\n".indent(authorization_header ? 12 : 8)
-          end
-        end
-
-        # Response
-        f.write "+ Response #{response.status} #{response.content_type}\n\n"
-
-        if response.body.present? && response.content_type == 'application/json'
-          f.write "#{JSON.pretty_generate(JSON.parse(response.body))}\n\n".indent(8)
-        end
-      end unless response.status == 401 || response.status == 403 || response.status == 301
+    file = File.join(api_docs_folder_path, "#{file_name}.md")
+    
+    new_file = false
+    if !touched_files[file_name]
+      File.delete(file) if File.exists?(file)
+      new_file = true
+      touched_files[file_name] = true
     end
+
+    File.open(file, 'a+') do |f|
+      # Resource
+      if new_file
+        f.puts "# Group #{resource_name}\n\n"
+
+        f.puts resource_comment
+      end
+
+      # Action
+      if new_file || !f.read.index(Regexp.new(Regexp.escape(action_match.to_s) + "$"))
+        f.puts "## #{action_match}"
+
+        f.puts action_comment
+      end
+
+      # Request
+      request_body = request.body.read
+      request_content_type = request.content_type
+      authorization_header = request.env ? request.env['Authorization'] : request.headers['Authorization']
+
+      if request_body.present? || authorization_header.present?
+        # Parse JSON body form style requests that simple rspec tests produce
+        if request_content_type == 'application/x-www-form-urlencoded'
+          request_content_type = 'application/json'
+          request_body = Rack::Utils.parse_nested_query(request_body).to_json
+        end
+
+        f.puts "+ Request #{example.metadata[:description_args].first} (#{request_content_type})\n\n"
+
+        # Request Headers
+        if authorization_header.present?
+          f.puts "+ Headers\n"
+          f.puts "Authorization: #{request_authorization_header}\n\n".indent(2)
+        end
+
+        # Request Body
+        if request_body.present? && request_content_type.include?('application/json')
+          f.puts "+ Body\n".indent(2) if authorization_header
+          f.puts "#{JSON.pretty_generate(JSON.parse(request_body))}\n\n".indent(authorization_header ? 6 : 4)
+        end
+      end
+
+      # Response
+      f.puts "+ Response #{response.status} (#{response.content_type})\n\n"
+
+      if response.body.present? && response.content_type.include?('application/json')
+        f.puts "#{JSON.pretty_generate(JSON.parse(response.body))}\n\n".indent(4)
+      end
+    end unless response.status == 401 || response.status == 403 || response.status == 301
+  end
+
+  private
+
+  # Go up in the example group (i.e. describe/context) hierarchy and try to match the text.
+  # @return matched example group, matched regex
+  def find_group(example_group, regex)
+    group = nil
+
+    begin
+      match = example_group[:description_args].first.match(regex)
+      example_group = example_group[:example_group] unless match
+    end while !match && example_group
+
+    return example_group, match
+  end
+
+  # Parse documentation from controller's comment
+  def parse_action_comment(example_group, folder)
+    return nil unless example_group
+
+    folder = folder.call(example_group[:file_path]) if folder.is_a? Proc
+    file_name = example_group[:file_path].match(/([a-zA-Z_-]+)_spec\.rb/)[1]
+
+    in_comment = false
+    comment_lines = []
+
+    File.open(File.join(folder, file_name.pluralize.underscore + '_controller.rb'), 'r').each do |line|
+      if in_comment
+        if line =~ /\s*# ?(.*)$/
+          comment_lines << $1
+        else
+          comment_lines << ""
+          break
+        end
+      elsif line =~ Regexp.new("\s*#\s*" + Regexp.escape(example_group[:description_args].first) + "\s*$")
+        in_comment = true
+      end
+    end
+
+    puts "Cannot find docs for action #{example_group[:description_args].first}" if comment_lines.size == 0
+
+    comment_lines
+  end
+
+  # Parse documentation from model's comment
+  def parse_resource_comment(example_group, folder)
+    return nil unless example_group
+
+    folder = folder.call(example_group[:file_path]) if folder.is_a? Proc
+    file_name = example_group[:file_path].match(/([a-zA-Z_-]+)_spec\.rb/)[1]
+
+    lines = File.read(File.join(folder, file_name.singularize.underscore + '.rb')).lines.to_a
+    row = 0
+
+    while row < lines.size
+      if lines[row].include?("class " + file_name.singularize.camelize)
+        row -= 1
+        break
+      else
+        row += 1
+      end
+    end
+
+    if row == lines.size
+      puts "Cannot find docs for resource #{file_name.singularize.camelize}"
+      return nil
+    end
+
+    comment_lines = [""]
+
+    while lines[row] =~ /\s*# ?(.*)$/
+      comment_lines.unshift($1)
+      row -= 1
+    end
+
+    comment_lines
   end
 end
